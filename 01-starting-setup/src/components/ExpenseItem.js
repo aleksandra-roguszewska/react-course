2b1fa7a4d44e8966da2967e7098ce313e200a989
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-05-04 16:48:50 +0200

    Complete lesson 37

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-
+export default function ExpenseItem({ date, title, amount }) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{date}</div>
       <div className="expense-item__description"></div>
-      <h2>{expenseTitle}</h2>
-      <div className="expense-item__price">${expenseAmount}</div>
+      <h2>{title}</h2>
+      <div className="expense-item__price">${amount}</div>
     </div>
   );
 }
