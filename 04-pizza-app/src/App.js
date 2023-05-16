55cda77e7c57d4b815e2b80a41d8830b4fdfc5db
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-05-16 13:51:26 +0200

    Improve Cart

--- 04-pizza-app/src/App.js
+++ 04-pizza-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
+import Cart from "./components/Cart/Cart1";
 import CartProvider from "./store/CartProvider";
 
 function App() {
---
+++ 04-pizza-app/src/components/Cart/Cart1.js
@@ -0,0 +1,56 @@
+import { useContext } from "react";
+import React from "react";
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+
+const Cart = (props) => {
+  const cartContext = useContext(CartContext);
+
+  const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+
+  const hasItems = cartContext.items.length > 0;
+
+  const cardItemRemoveHandler = (id) => {};
+
+  const cardItemAddHandler = (item) => {};
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartContext.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cardItemRemoveHandler.bind(null, item.id)}
+          onAdd={cardItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.hideCartHandler}>
+      <div>
+        {cartItems}
+        <div className={classes.total}>
+          <span>Total Amount</span>
+          <span>{totalAmount}</span>
+        </div>
+        <div className={classes.actions}>
+          <button
+            className={classes["button--alt"]}
+            onClick={props.hideCartHandler}
+          >
+            Close
+          </button>
+          {hasItems && <button className={classes.button}>Order</button>}
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
