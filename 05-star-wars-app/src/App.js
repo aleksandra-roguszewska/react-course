dab3c1efebb9ce51a376c3126f4c505d671ddb7b
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-05-17 16:02:18 +0200

    Finish starwars-app

--- 05-star-wars-app/src/App.js
+++ 05-star-wars-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -26,7 +26,7 @@ function App() {
   //     });
   // }
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -50,7 +50,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>
