b205741ff82fdc900c414fccc320e94ccf809498
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-05-26 13:37:43 +0200

    Import course files

--- 10-pizza-app-continuation/src/components/Meals/AvailableMeals.js
+++ 10-pizza-app-continuation/src/components/Meals/AvailableMeals.js
@@ -11,7 +11,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://course-project-f5644-default-rtdb.firebaseio.com/meals"
+        "https://course-project-f5644-default-rtdb.firebaseio.com/meals.json"
       );
 
       if (!response.ok) {
